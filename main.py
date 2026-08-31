814fdaac4fdca2d582e1fec05c730d63fe00109f
Arianna Mora <arigmorav@gmail.com>
2026-08-31 14:01:34 -0500

    configuración para modo sin interfaz

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.browser import crear_navegador
 from src.scraper import extraer_noticias
+from src.exporter import export_to_csv
 
 
 def main():
-    navegador = crear_navegador()
+    navegador = crear_navegador(headless=True)
 
     navegador.get("http://localhost:5500/src/test_page.html")
 
@@ -11,10 +12,13 @@ def main():
 
     navegador.quit()
 
-    print(f"Noticias encontradas: {len(noticias)}")
+    export_to_csv(
+        noticias,
+        "data/noticias.csv"
+    )
 
-    for noticia in noticias:
-        print(noticia["title"])
+    print(f"Noticias encontradas: {len(noticias)}")
+    print("Archivo CSV generado correctamente.")
 
 
 if __name__ == "__main__":
--- src/browser.py
+++ src/browser.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-def crear_navegador():
-    navegador = webdriver.Chrome()
+def crear_navegador(headless=False):
+    opciones = Options()
+
+    if headless:
+        opciones.add_argument("--headless")
+
+    navegador = webdriver.Chrome(options=opciones)
+
     return navegador
